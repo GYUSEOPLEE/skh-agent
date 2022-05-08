333952021fb5165c69eea8991a4d2ba05bf69ecd
Lee Gyu Seop <gseoplee@gmail.com>
2022-05-08 18:10:14 +0900

    Initial commit

---
+++ src/main/java/kr/co/skh/agent/SkhAgentApplication.java
@@ -0,0 +1,13 @@
+package kr.co.skh.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkhAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkhAgentApplication.class, args);
+    }
+
+}
