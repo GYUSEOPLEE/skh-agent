6ef9ebc8d1c2ac78ece54078e19867202e6b8503
hyeongkyu <hyeongkyuhan@gmail.com>
2022-06-13 19:33:32 +0900

    test코드 수정

--- src/test/java/kr/co/skh/agent/SkhAgentApplicationTests.java
+++ src/test/java/kr/co/skh/agent/SkhAgentApplicationTests.java
@@ -3,14 +3,9 @@ package kr.co.skh.agent;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDateTime;
-
-
 @SpringBootTest
 class SkhAgentApplicationTests {
     @Test
     void contextLoads() {
-        LocalDateTime localDateTime = LocalDateTime.now().withNano(0);
-        System.out.println(localDateTime);
     }
 }
